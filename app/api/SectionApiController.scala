32d181f8ca7d8101506327d5d20b173b0e9e2690
Grzegorz Jasinski <gjasinski@hotmail.com>
2017-06-28 17:51:28 +0200

    Use FieldDTO in SectionApiController

--- app/api/SectionApiController.scala
+++ app/api/SectionApiController.scala
@@ -2,9 +2,9 @@ package api
 
 import javax.inject.Inject
 
+import controllers.EventPagesController.FieldDTO
 import dao.{FieldDAO, SectionDAO}
 import models._
-import play.Logger
 import play.api.libs.json._
 import play.api.mvc._
 
@@ -12,19 +12,32 @@ import scala.concurrent.{ExecutionContext, Future}
 
 
 class SectionApiController @Inject()(sectionDAO: SectionDAO, fieldDAO: FieldDAO)(implicit ec: ExecutionContext) extends Controller {
-   def getSectionsByPageId(id: Long) = Action.async {
-    implicit request =>{
+  implicit val fieldDTOWrites = new Writes[FieldDTO] {
+    def writes(field: FieldDTO) = Json.obj(
+      "fieldtype" -> field.fieldType,
+      "content" -> field.content,
+      "oridinal" -> field.ordinal,
+      "url" -> field.url,
+      "description" -> field.description
+    )
+  }
+
+  def getSectionsByPageId(id: Long) = Action.async {
+    implicit request => {
       val sections = sectionDAO.findByPageId(id)
       val images = fieldDAO.getAllImages
       val headings = fieldDAO.getAllHeadings
       val paragraphs = fieldDAO.getAllParagraphs
-      for{
+      for {
         s <- sections
         i <- images
         h <- headings
         p <- paragraphs
-      }yield {
-        if(s.isEmpty){
+      } yield {
+        val fieldDTOs = i.map(image => FieldDTO(FieldType.Image, "", image.ordinal, image.url, image.description)) ++
+          h.map(heading => FieldDTO(FieldType.Heading, heading.content, heading.ordinal, "", "")) ++
+          p.map(paragraph => FieldDTO(FieldType.Paragraph, paragraph.content, paragraph.ordinal, "", ""))
+        if (s.isEmpty) {
           NotFound(Json.arr())
         }
         else {
@@ -35,37 +48,12 @@ class SectionApiController @Inject()(sectionDAO: SectionDAO, fieldDAO: FieldDAO)
                 "pageid" -> s.pageId,
                 "ordinal" -> s.ordinal,
                 "title" -> s.title,
-                getJsonWithFields(i, p, h, s.id.get)
+                "fields" -> Json.toJson(fieldDTOs)
               ))
             }
           }.reduce((s1, s2) => s1 ++ s2))
         }
       }
     }
-  }
-
-  def getJsonWithFields(images:Seq[Image], paragraphs:Seq[Paragraph], headings:Seq[Heading], id: Long): (String, Json.JsValueWrapper)={
-    val t1 = images.filter(_.sectionId == id).map(image => Json.arr(Json.obj(
-      "id" -> image.id,
-      "pageid" -> image.sectionId,
-      "ordinal" -> image.ordinal,
-      "url" -> image.url,
-      "description:" -> image.description)
-    ))
-    val t3 = headings.filter(_.sectionId == id).map(heading => Json.arr(Json.obj(
-      "id" -> heading.id,
-      "pageid" -> heading.sectionId,
-      "ordinal" -> heading.ordinal,
-      "title" -> heading.content)
-    ))
-    val t2 = paragraphs.filter(_.sectionId == id).map(paragraph => Json.arr(Json.obj(
-      "id" -> paragraph.id,
-      "pageid" -> paragraph.sectionId,
-      "ordinal" -> paragraph.ordinal,
-      "title" -> paragraph.content)
-    ))
-    val t4 = t1 ++ t2 ++ t3
-    if(t4.nonEmpty) "fields" -> t4.reduce((x,y) => x ++ y)
-    else "fields" ->  Json.arr()
   }
 }
